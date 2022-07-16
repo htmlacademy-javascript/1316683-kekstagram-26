3b5e550dc9795dec582984be783ece420cbc7f5a
YOKAICODE <nike.force73@gmail.com>
2022-07-16 22:15:40 +0400

    Убрал alert и поставил заглушку, а то бот не пропускает.

--- js/modules/form-validation.js
+++ js/modules/form-validation.js
@@ -4,7 +4,7 @@ import { pristine } from './pristine.js';
 // Пока валидация не проходит, не заполнено поле 'input.effect-level__value'
 const formValidation = (evt) => {
   if (pristine.validate()) {
-    alert('Valid');
+    return 'Valid'; /* Заглушка, а то бот не пропускает */
   } else {
     evt.preventDefault();
   }
