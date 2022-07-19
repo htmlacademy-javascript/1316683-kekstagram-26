e030c3cb5458f9104f59e9346158271a3ca042e6
YOKAICODE <nike.force73@gmail.com>
2022-07-19 19:46:12 +0400

    Полное написание модуля изменения масштаба превью

--- js/modules/editor.js
+++ js/modules/editor.js
@@ -2,15 +2,21 @@ const formElement = document.querySelector('form#upload-select-image');
 const addScaleElement = formElement.querySelector('.scale__control--bigger');
 const reduceScaleElement = formElement.querySelector('.scale__control--smaller');
 const valueScaleElement = formElement.querySelector('.scale__control--value');
+const previewImageElement = formElement.querySelector('.img-upload__preview > img');
 const SCALE_STEP = 25;
 const MAX_SCALE_VALUE = 100;
 const MIN_SCALE_VALUE = 25;
 const DEFAULT_SCALE_VALUE = 50;
 
+const updateScaleImage = () => {
+  previewImageElement.style.transform = `scale(${parseInt(valueScaleElement.value, 10) / 100})`;
+};
+
 // Увеличение масштаба
 const addScale = () => {
   if (parseInt(valueScaleElement.value, 10) !== MAX_SCALE_VALUE) {
     valueScaleElement.value = `${parseInt(valueScaleElement.value, 10) + SCALE_STEP}%`;
+    updateScaleImage();
   }
 };
 
@@ -18,6 +24,7 @@ const addScale = () => {
 const reduceScale = () => {
   if (parseInt(valueScaleElement.value, 10) !== MIN_SCALE_VALUE) {
     valueScaleElement.value = `${parseInt(valueScaleElement.value, 10) - SCALE_STEP}%`;
+    updateScaleImage();
   }
 };
 
@@ -25,6 +32,7 @@ const initScale = () => {
   valueScaleElement.value = `${DEFAULT_SCALE_VALUE}%`;
   addScaleElement.addEventListener('click', addScale);
   reduceScaleElement.addEventListener('click', reduceScale);
+  updateScaleImage();
 };
 
 const removeScaleHandler = () => {
